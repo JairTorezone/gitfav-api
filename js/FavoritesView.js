766912998d8308f82f5727fbb6be9f09abb75332
Jair Torezone <jairtorezone@gmail.com>
2023-10-29 17:41:02 -0300

    created method onadd

--- js/FavoritesView.js
+++ js/FavoritesView.js
@@ -6,6 +6,26 @@ export class FavoritesView extends Favorites {
 
     this.tbody = this.root.querySelector("table tbody");
     this.update();
+    this.onadd();
+  }
+
+  // onadd() {
+  //   const addButton = this.root.querySelector(".search button");
+  //   addButton.onclick = () => {
+  //     const { value } = this.root.querySelector(".search input");
+
+  //     this.add(value);
+  //   };
+  // }
+
+  onadd() {
+    const addButton = this.root.querySelector(".search button");
+
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector(".search input");
+
+      this.add(value);
+    };
   }
 
   update() {
