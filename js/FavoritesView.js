0107f233fea238b45dde7185d1012121e2e4e160
Jair Torezone <jairtorezone@gmail.com>
2023-10-29 10:05:25 -0300

    created methods createRow and createRowEmpty

--- js/FavoritesView.js
+++ js/FavoritesView.js
@@ -7,6 +7,43 @@ export class FavoritesView extends Favorites {
     this.tbody = this.root.querySelector("table tbody");
   }
 
+  createRow() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+            <td class="user">
+              <img
+                src="https://github.com/jairtorezone.png"
+                alt="Imagem do Jair"
+              />
+              <a href="https://github.com/jairtorezone" target="_blank">
+                <p>Jair Torezone</p>
+                <span>/jairTorezone</span>
+              </a>
+            </td>
+            <td class="repository">123</td>
+            <td class="followers">1234</td>
+            <td class="action">
+              <button class="remove">Remover</button>
+            </td>
+          `;
+    return tr;
+  }
+
+  createRowEmpty() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+            <td colspan="4" class="table-empty">
+              <div>
+                <img src="./assets/img/Estrela.svg" alt="" />
+                <p>Nenhum favorito ainda</p>
+              </div>
+            </td>
+          `;
+    return tr;
+  }
+
   removeTrAll() {
     this.tbody.querySelectorAll("tr").forEach((element) => {
       element.remove();
